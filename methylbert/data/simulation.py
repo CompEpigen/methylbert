b304c7f35ef480054fe3259ec87c5d6dd90f9281
hanyangii <y.jeong@dkfz-heidelberg.de>
2023-01-23 15:51:45 +0100

    read simulation hyperparameters and pseudo-bulks

--- methylbert/data/simulation.py
+++ methylbert/data/simulation.py
@@ -8,15 +8,24 @@ import pandas as pd
 
 def arg_parser():
     parser = argparse.ArgumentParser()
+    # Required hyperparameters 
     parser.add_argument("-d", "--f_dmr", required=True, type=str, help="tab-separated .csv file, DMRs should be given with mean methylation level of each cell type, chr, start and end")
     #parser.add_argument("-i", "--f_input", required=False, type=str, help=".csv file, DMRs should be given with mean methylation level of each cell type, chr, start and end")
     parser.add_argument("-o", "--output_dir", required=True, type=str, help="a directory where all generated results will be saved")
     parser.add_argument("-r", "--f_ref", required=True, type=str, help=".fasta file for the reference genome")
+
+    #Hyperparameters for read-level methylome simulation 
     parser.add_argument("-nm", "--n_mers", type=int, default=3, help="n-mers")
     parser.add_argument("-nr", "--n_reads", type=int, default=120, help="Read coverage to simulate in each DMR")
     parser.add_argument("-l", "--len_read", type=int, default=150, help="Read length to simulate")
     parser.add_argument("--seed", type=int, default=950410, help="seed number")
+
+    # Hyperparameters for pseudo-bulks 
     parser.add_argument("--bulk", type=bool, default=True, help="Whether you want to generate pseudo-bulks or the entire dataset")
+    parser.add_argument("-nb", "--n_bulks", type=int, default=None, help="Number of bulks, Applicable only when --bulk is True")
+    parser.add_argument("-s", "--std", type=float, default=0.0, help="Standard deviation to sample local proportions. The larger value is given, the more varied local proportions are sampled from a Gaussian distribution centred at the global proportion")
+    parser.add_argument("-t", "--f_gt", type=str, help="tab-separated .csv file, each column should be a proportion for each cell-type. bulk name can be included as dataframe index/row name, only used when n_bulks is not given")
+    
 
     return parser.parse_args()
 
@@ -30,6 +39,10 @@ def simulation(args):
     if not os.path.exists(args.output_dir):
         os.mkdir(args.output_dir)
 
+    # Save hyperparameters 
+    pd.DataFrame({"param":vars(args).keys(),
+                  "values":vars(args).values()}).to_csv(args.output_dir+"/parameters.txt", sep="\t")
+
     # Reference genome
     record_iter = SeqIO.parse(args.f_ref, "fasta")
 
@@ -38,11 +51,6 @@ def simulation(args):
         dict_ref[r.id] = str(r.seq.upper())
     del record_iter
 
-    # "dna_seq", "methyl_seq", "dmr_ctype", "ctype", "dmr_label", "ref_pos"
-    #n_reads = 120
-    #len_read = 150
-    #res_dir="/home/yuni/icgc/DL_project/methylbert/data/bcell_lymphoma_synthesis_nonbias/"
-    
     # Load DMRs
     df_dmr = pd.read_csv(args.f_dmr, sep="\t")
     if "dmr_id" in df_dmr.keys():
@@ -101,14 +109,25 @@ def simulation(args):
         reads.to_csv(f"{args.output_dir}/data.txt", sep="\t", index=False)
     else:
         # pseudo_bulk simulation
-        n_bulks = 20
-        std=0
-
-        ratios = np.random.uniform(size=n_bulks)
         ctypes = reads["ctype"].unique()
-        for bulk_idx in tqdm(range(n_bulks)):
+        
+        if not args.n_bulks:
+            df_gt = pd.read_csv(args.f_gt, sep="\t")
+            print(df_gt)
+            pri_ratios = df_gt["T"].tolist()
+            bulk_names = df_gt["bulk"] if "bulk" in df_gt.keys() else df_gt.index
+            bulk_names = bulk_names.tolist()
+            n_bulks = df_gt.shape[0]
+        else:
+            pri_ratios = np.random.uniform(size=args.n_bulks)
+            n_bulks = args.n_bulks
+            bulk_names = ["bulk_%d"%i for i in range(n_bulks)]
+
+        post_ratios = list()
+
+        for bulk_idx, bulk_name in tqdm(enumerate(bulk_names)):
             # proportion for each dmr
-            local_proportions = np.random.normal(loc=ratios[bulk_idx], scale=std, size=df_dmr.shape[0])
+            local_proportions = np.random.normal(loc=pri_ratios[bulk_idx], scale=args.std, size=df_dmr.shape[0])
             bulk_reads = list()
             
             for dmr_idx in range(df_dmr.shape[0]):
@@ -119,7 +138,13 @@ def simulation(args):
                     sampled_idces = random.sample(dmr_reads.loc[dmr_reads["ctype"]==ctype].index.tolist(), int((args.n_reads/len(ctypes)) * ctype_ratio))
                     bulk_reads.append(dmr_reads.loc[sampled_idces, ])
             bulk_reads = pd.concat(bulk_reads)
-            bulk_reads.to_csv(f"{args.output_dir}/bulk_{bulk_idx}.txt", sep="\t", index=False)
+            bulk_reads.to_csv(f"{args.output_dir}/{bulk_name}.txt", sep="\t", index=False)
+            post_ratios.append((bulk_reads["ctype"]=="T").sum()/bulk_reads.shape[0])
+
+        gt_ratios = pd.DataFrame({"bulk":bulk_names,
+                                "T": post_ratios,
+                                "N": [1-r for r in post_ratios]})
+        gt_ratios.to_csv(f"{args.output_dir}/gt_ratio.csv", sep="\t", index=False)
 
 
 if __name__=="__main__":
