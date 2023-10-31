a6abffb3f1e6080d5fd9daf836f72ddf1b30155b
hanyangii <y.jeong@dkfz-heidelberg.de>
2023-10-31 11:03:33 +0100

    setup file

--- setup.py
+++ setup.py
@@ -2,5 +2,18 @@ from setuptools import setup
 
 setup(name='methylbert',
     version='0.0.1',
+
 	packages=['methylbert'],
+	package_dir = {"": "src"},
+
+	author='Yunhee Jeong',
+	author_email='y.jeong@dkfz-heidelberg.de',
+
+	# cli
+	entry_points={
+		"console_scripts": [
+			"methylbert = cli:main"
+		]
+    }
+
 )
