d8f67e378c94c694d432b50f61f6b494b6c0fd9f
hanyangii <y.jeong@dkfz-heidelberg.de>
2023-11-02 16:52:19 +0100

    setup update

--- setup.py
+++ setup.py
@@ -1,10 +1,31 @@
 from setuptools import setup
+from setuptools import find_packages
+
+import os, glob
 
 setup(name='methylbert',
-    version='0.0.1',
+	version='0.0.1',
 
-	packages=['methylbert'],
+	packages=find_packages("src"),
 	package_dir = {"": "src"},
+
+	install_requires=[
+		'Bio', 
+		'matplotlib',
+		'numpy',
+		'pandas',
+		'pysam',
+		'scikit_learn',
+		'scipy',
+		'torch>=1.10.0',
+		'tqdm',
+		'transformers>=2,<3'
+	],
+
+    py_modules=[os.path.splitext(os.path.basename(path))[0] for path in glob.glob("src/*.py")]+ \
+    		   [os.path.splitext(os.path.basename(path))[0] for path in glob.glob("src/methylbert/*.py")],
+
+	test_suite="test",
 
 	author='Yunhee Jeong',
 	author_email='y.jeong@dkfz-heidelberg.de',
@@ -14,6 +35,6 @@ setup(name='methylbert',
 		"console_scripts": [
 			"methylbert = cli:main"
 		]
-    }
+	}
 
 )
