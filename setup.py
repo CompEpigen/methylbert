537557a05e47f4e9933c5eeb41b3600a86232a12
Yunhee Jeong <y.jeong@dkfz-heidelberg.de>
2024-02-09 11:07:44 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import os, glob, warnings
 warnings.filterwarnings("ignore")
 
 setup(name='methylbert',
-	version='0.0.1',
+	version='0.0.2',
 
 	packages=find_packages("src"),
 	package_dir = {"": "src"},
