89f4d213e878c545a6729fbe899f74429a59a075
hanyangii <y.jeong@dkfz-heidelberg.de>
2024-08-15 17:34:55 +0200

    update test cases

--- test/test_finetune_preprocess.py
+++ test/test_finetune_preprocess.py
@@ -102,19 +102,50 @@ def test_dorado_aligned_file(bam_file: str, f_dmr: str, f_ref: str, out_dir = "t
 
 	print("test_dorado_aligned_file passed!")
 
+
+def test_multi_cell_type(f_bam_file_list: str, f_dmr: str, f_ref: str, out_dir = "tmp/"):
+	fdg.finetune_data_generate(
+		sc_dataset = f_bam_file_list,
+		f_dmr = f_dmr,
+		f_ref = f_ref,
+		output_dir=out_dir,
+		split_ratio = 0.8,
+		n_cores=1
+	)
+
+	assert os.path.exists(out_dir+"train_seq.csv")
+	assert os.path.exists(out_dir+"test_seq.csv")
+	assert os.path.exists(out_dir+"dmrs.csv")
+
+	res  = pd.read_csv(out_dir+"train_seq.csv", sep="\t")
+	assert "T" in res["ctype"].tolist()
+	assert "N" in res["ctype"].tolist()
+	assert "P" in res["ctype"].tolist()
+
+	print("test_multi_cell_type passed!")
+
+
 if __name__=="__main__":
 	f_bam = "data/T_sample.bam"
 	f_bam_list = "data/bam_list.txt"
 	f_dmr = "data/dmrs.csv"
 	f_ref = "data/genome.fa"
 
+	
 	test_single_bam_file(bam_file = f_bam, f_dmr=f_dmr, f_ref=f_ref)
 	test_list_bam_file(f_bam_file_list = f_bam_list, f_dmr=f_dmr, f_ref=f_ref)
 	test_dmr_subset(bam_file = f_bam, f_dmr=f_dmr, f_ref=f_ref, n_dmrs=10)
 	test_multi_cores(bam_file = f_bam, f_dmr=f_dmr, f_ref=f_ref, n_cores=4)
 	test_split_ratio(bam_file = f_bam, f_dmr=f_dmr, f_ref=f_ref, split_ratio=0.7)
+	
 
+	f_bam_list = "data/multi_cell_type/bam_list.txt"
+	f_dmr = "data/multi_cell_type/dmrs.csv"
+	out_dir = "data/multi_cell_type/"
+	test_multi_cell_type(f_bam_file_list = f_bam_list, f_dmr=f_dmr, f_ref=f_ref, out_dir=out_dir)
+	
 	f_dorado = "data/dorado_aligned.bam"
 	f_ref_hg38="data/hg38_genome.fa"
 	test_dorado_aligned_file(bam_file = f_dorado, f_dmr=f_dmr, f_ref=f_ref_hg38)
 	print("Everything passed!")
+	
