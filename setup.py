c5c42ddb03bfb06a7a096e91a3150905091e3dd2
hanyangii <y.jeong@dkfz-heidelberg.de>
2024-09-30 15:43:27 +0200

    version update

--- setup.py
+++ setup.py
@@ -5,28 +5,31 @@ import os, glob, warnings
 
 warnings.filterwarnings("ignore")
 
-setup(name='methylbert',
-	version='1.0.0',
-
-	packages=find_packages("src"),
-	package_dir = {"": "src"},
-
-	install_requires=[
+requirements=[
 		'Bio', 
 		'biopython<=1.81',
-		'matplotlib<3.3',
+		'matplotlib<3.3,>=3.2',
 		'numpy<1.21',
 		'pandas<1.4.0',
 		'pysam',
 		'scikit_learn<1.1.0',
-		'scipy<1.7.0',
-		'torch>=1.10.0',
+		#'scipy<1.7.0',
+		'torch>=2.4.0',
 		'tqdm',
-		'transformers>=2,<2.6',
-		'tokenizers<0.6.0',
+		'transformers>=4.0.0',
+		'tokenizers<0.20.0',
 		'urllib3<1.27,>=1.25.4',
 		'zipp==3.13.0'
-	],
+	]
+
+setup(name='methylbert',
+	version='2.0.0',
+
+	packages=find_packages("src"),
+	package_dir = {"": "src"},
+
+	install_requires = requirements,
+	setup_requires = requirements,
 
     py_modules=[os.path.splitext(os.path.basename(path))[0] for path in glob.glob("src/*.py")]+ \
     		   [os.path.splitext(os.path.basename(path))[0] for path in glob.glob("src/methylbert/*.py")],
