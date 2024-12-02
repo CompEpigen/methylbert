fd11f1f835d17dfa52fd3d699b7727261318ed34
hanyangii <y.jeong@dkfz-heidelberg.de>
2024-12-02 12:52:11 +0100

    trian test split ratio and cell type stratification

--- src/methylbert/data/finetune_data_generate.py
+++ src/methylbert/data/finetune_data_generate.py
@@ -169,7 +169,9 @@ def finetune_data_generate(
         sc_dataset: str = None,
         input_file: str = None,
         n_mers: int = 3,
-        split_ratios: List[float] = [0.8, 0.1, 0.1],
+        split_ratio: float = None,
+        train_valid_test_ratio: List[float] = None,
+        use_file_name: bool = False,
         n_dmrs: int = -1,
         n_cores: int = 1,
         seed: int = 950410,
@@ -182,6 +184,19 @@ def finetune_data_generate(
     # Setup random seed
     random.seed(seed)
     np.random.seed(seed)
+
+    # Check split ratio 
+    if (split_ratio is not None) and (train_valid_test_ratio is not None):
+        raise ValueError("Only either of 'split_ratio (float)' or train_valid_test_ratio 'List[float]' must be given.")
+    elif split_ratio is not None:
+        split_ratios = [split_ratio, 1-split_ratio, 0.0]
+    elif train_valid_test_ratio is not None:
+        if ( np.sum(train_valid_test_ratio) != 1.0 ) or \
+            ( len(train_valid_test_ratio) != 3 ):
+            raise ValueError("'train_valid_test_ratio' must be a list with 3 float values whose sum is 1.0")
+        split_ratios = train_valid_test_ratio
+    else:
+        split_ratios = [1.0, 0.0, 0.0] # output must be one single file
 
     # Setup output files
     if not os.path.exists(output_dir):
@@ -199,7 +214,9 @@ def finetune_data_generate(
 
     # Load DMRs into a dataframe
     dmrs = pd.read_csv(f_dmr, sep="\t", index_col=None)
-    if ("chr" not in dmrs.keys()) or ("start" not in dmrs.keys()) or ("end" not in dmrs.keys()):
+    if ("chr" not in dmrs.keys()) or \
+       ("start" not in dmrs.keys()) or \
+       ("end" not in dmrs.keys()):
         ValueError("The .csv file for DMRs must contain chr, start and end in the header.")
 
     # Remove chrX, chrY, chrM and so on in DMRs
@@ -268,18 +285,28 @@ def finetune_data_generate(
     elif ( not sc_dataset ):
         # one input file in the list
         sc_files = [input_file]
+        if use_file_name:
+            if verbose > 0:
+                print('When only one input file is give, file name cannot be used for the train-test split. We set use_file_name=False. Read names will be used for the split')
+            use_file_name = False
     elif ( not input_file ):
         # Collect train data (single-cell samples)
         train_sc_samples = []
         with open(sc_dataset, "r") as fp_sc_dataset:
             sc_files = fp_sc_dataset.readlines()
+
+        if ( len(sc_files) < 10 ) and ( use_file_name ):
+            warnings.warn("We do not encourage to users to set use_file_name=True with the number of input bam files < 10. It can cause an unexpected error.")
     else:
-        sys.exit("Either a list of input files or a file path must be given.")
+        raise ValueError("Either a list of input files or a file path must be given.")
 
     # Collect reads from the .bam files
     df_reads = list()
-    tqdm_bar = tqdm(total=len(sc_files), desc="Collecting reads from .bam files")
-    files_lbl_map = {}
+    tqdm_bar = tqdm(total=len(sc_files), 
+                    desc="Collecting reads from .bam files")
+    
+    # file/read name - cell type pair for stratification in train test split
+    files_lbl_map = {} 
     for f_sc in sc_files:
         f_sc = f_sc.strip().split("\t")
         f_sc_bam = f_sc[0]
@@ -313,13 +340,20 @@ def finetune_data_generate(
             extracted_reads["ctype"] = f_sc[1]
         else:
             extracted_reads["ctype"] = "NA"
-        extracted_reads = extracted_reads.rename(columns={"RF":"dna_seq", "ME":"methyl_seq"})
+        extracted_reads = extracted_reads.rename(columns={"RF":"dna_seq", 
+                                                          "ME":"methyl_seq"})
 
         if(extracted_reads.shape[0] > 0):
-            filename = os.path.basename(f_sc_bam)
-            if len(f_sc) > 1:
-                files_lbl_map[filename] = f_sc[1]  # label of the file
-            extracted_reads["filename"] = filename
+            if use_file_name:
+                filename = os.path.basename(f_sc_bam)
+                if len(f_sc) > 1:
+                    files_lbl_map[filename] = extracted_reads['ctype'][0]  
+                extracted_reads["filename"] = filename
+            else:
+                for name, ctype in zip(extracted_reads['name'], 
+                                       extracted_reads['ctype']):
+                    files_lbl_map[name] = ctype
+
             df_reads.append(extracted_reads)
 
         tqdm_bar.update()
@@ -329,6 +363,7 @@ def finetune_data_generate(
         df_reads = pd.concat(df_reads, ignore_index=True) \
             .sample(frac=1) \
             .reset_index(drop=True) # sample is for shuffling
+
         if verbose > 1:
             print("Fine-tuning data generated:", df_reads.head())
     else:
@@ -339,10 +374,11 @@ def finetune_data_generate(
         print(df_reads["ctype"].value_counts())
 
     # Split the data into train and train/valid/test by patient/bam file
-    if np.sum(split_ratios) == 1 and len(split_ratios) == 3:
+    if split_ratios[0] != 1.0:
         fp_train_seq = os.path.join(output_dir, "train_seq.csv")
-        fp_val_seq = os.path.join(output_dir, "val_seq.csv")
         fp_test_seq = os.path.join(output_dir, "test_seq.csv")
+
+        split_key = "filename" if use_file_name else "name"
 
         val_test_size = 1 - split_ratios[0]
         train_files, test_files = train_test_split(
@@ -351,27 +387,29 @@ def finetune_data_generate(
             stratify=list(files_lbl_map.values())
         )
 
-        test_size = split_ratios[2] / (split_ratios[1] + split_ratios[2])
-        val_files, test_files = train_test_split(
-            test_files,
-            test_size=test_size, random_state=seed,
-            stratify=[files_lbl_map[e] for e in test_files]
-        )
+        if split_ratios[-1] > 0.0:
+            fp_val_seq = os.path.join(output_dir, "val_seq.csv")
+            test_size = split_ratios[2] / (split_ratios[1] + split_ratios[2])
+            val_files, test_files = train_test_split(
+                test_files,
+                test_size=test_size, random_state=seed,
+                stratify=[files_lbl_map[e] for e in test_files]
+            )
 
-        # TODO: we should somehow account for ctype stratification
-
+            df_reads.loc[df_reads[split_key].isin(val_files), :] \
+            .to_csv(fp_val_seq, sep="\t", header=True, index=None)
+        
         # Write train & test files (adding a final column because of sep="\t")
         df_reads["non_null_col"] = ""
-        df_reads.loc[df_reads["filename"].isin(train_files), :] \
+        df_reads.loc[df_reads[split_key].isin(train_files), :] \
             .to_csv(fp_train_seq, sep="\t", header=True, index=None)
-        df_reads.loc[df_reads["filename"].isin(val_files), :] \
-            .to_csv(fp_val_seq, sep="\t", header=True, index=None)
-        df_reads.loc[df_reads["filename"].isin(test_files), :] \
+        df_reads.loc[df_reads[split_key].isin(test_files), :] \
             .to_csv(fp_test_seq, sep="\t", header=True, index=None)
 
         if verbose > 0:
-            print("Size - train %d seqs , valid %d seqs "%(df_reads.loc[df_reads["filename"].isin(train_files), :].shape[0],
-                                                           df_reads.loc[df_reads["filename"].isin(test_files), :].shape[0]))
+            print("Size - train %d seqs , valid %d seqs "% \
+                (df_reads.loc[df_reads[split_key].isin(train_files), :].shape[0],
+                 df_reads.loc[df_reads[split_key].isin(test_files), :].shape[0]))
 
     else:
         fp_data_seq = os.path.join(output_dir, "data.csv")
